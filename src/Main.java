812c9e9bef20918671e48d6b292b3ad6f088e370
User <esipov@sv-en.ru>
2024-02-22 10:42:51 +0300

    Первый файл

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+
+
+        while (true) {
+
+            int number = 0;
+            System.out.println("Введите строку");
+            Scanner scanner = new Scanner(System.in);
+            String text = scanner.nextLine();
+
+            Pattern pattern = Pattern.compile("[0-9]+");
+            Matcher matcher = pattern.matcher(text);
+            while (matcher.find()) {
+                number = Integer.parseInt(matcher.group())+number;
+                }
+            System.out.println(number);
+
+        }
+    }
+}
