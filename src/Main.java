2450ec381118ba99d839d71ec6b5553d138c499d
User <esipov@sv-en.ru>
2024-02-22 11:43:53 +0300

    Второй файл

--- src/Main.java
+++ src/Main.java
@@ -24,6 +24,7 @@ public class Main {
                 number = Integer.parseInt(matcher.group())+number;
                 }
             System.out.println(number);
+            System.out.println("2222");
 
         }
     }
